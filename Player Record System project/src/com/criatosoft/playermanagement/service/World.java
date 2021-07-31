d40e6ac619b57e74c54d3f88e41777d3e30d198d
maheshgujar91 <91maheshgujar@gmail.com>
2021-07-31 17:16:09 +0530

    Utkarsh Project
    Resolved

---
+++ Player Record System project/src/com/criatosoft/playermanagement/service/World.java
@@ -0,0 +1,11 @@
+package com.criatosoft.playermanagement.service;
+
+import java.io.IOException;
+
+public interface World {
+
+	public void addPlayer() throws IOException;
+	public void viewPlayer() throws IOException;
+	public void viewScore() throws IOException ;
+	public void updateScore() throws IOException;
+}
